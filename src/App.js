fbf1e7bf1bf779b77d07a0460d8ea204a058f54a
Alejandro <alejandro.becerra.pro@gmail.com>
2021-03-01 09:52:09 -0500

    obteniendo la letra de la cancion por medio de la API con el uso de Axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import {Fragment} from 'react';
+import {Fragment,useState,useEffect} from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
+
 function App() {
+
+  //definir el state
+
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra,guardarLetra] = useState('');
+
+  useEffect(() => {
+    
+    if(Object.keys(busquedaletra).length === 0) return ;
+    
+    const consultarApiLetra = async () => {
+      const {artista,cancion} = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+
+      guardarLetra(resultado.data.lyrics)
+    }
+    consultarApiLetra();
+
+  }, [busquedaletra])
+
   return (
     <Fragment>
       <Formulario 
-      
+        guardarBusquedaLetra = {guardarBusquedaLetra}
       />
     </Fragment>
   );
